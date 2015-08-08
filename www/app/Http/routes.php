b23ceec6cd1e64d46486d47cac4058d4fb29e91d
Ant <antonyharfield@gmail.com>
2015-08-08 16:56:52 +0700

    Added info page.

--- www/app/Http/routes.php
+++ www/app/Http/routes.php
@@ -11,8 +11,11 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', function() {
     return view('welcome');
+});
+Route::get('info', function() {
+    return view('info');
 });
 
 Route::get('api/questions', 'ApiController@getQuestions');
